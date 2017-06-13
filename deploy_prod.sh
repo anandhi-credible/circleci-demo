01eb3a59a81427fed79f196ad89ed2ecaef67d74
anandhi-credible <anandhim@credible.com>
2017-06-13 12:50:57 -0700

    First commit

---
+++ deploy_prod.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+ssh -v root@192.168.2.204 << EOF
+  
+  echo '1. Updating sources'
+  cd /anandhi-credible/circleci-demo/
+  git checkout --force master
+  git pull
+
+  echo "2. Restart apache"
+  sudo apache2ctl graceful
+
+  echo 'Done!'
+  echo 'Done!'
+  
+EOF
